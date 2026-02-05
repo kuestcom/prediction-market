78feb5d3f71a386a972ea92d7e57c3e3153c7b2d
ibruno <bruno@maciel.com>
2026-02-05 13:21:27 -0300

    fix: remove unsupported dynamic config

--- src/app/[locale]/(platform)/leaderboard/[[...filters]]/page.tsx
+++ src/app/[locale]/(platform)/leaderboard/[[...filters]]/page.tsx
@@ -3,8 +3,6 @@ import { setRequestLocale } from 'next-intl/server'
 import LeaderboardClient from '@/app/[locale]/(platform)/leaderboard/_components/LeaderboardClient'
 import { parseLeaderboardFilters } from '@/app/[locale]/(platform)/leaderboard/_utils/leaderboardFilters'
 
-export const dynamic = 'force-dynamic'
-
 export const metadata: Metadata = {
   title: 'Leaderboard',
 }
