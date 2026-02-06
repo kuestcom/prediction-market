4ac44b3311fdde2698ccb01fb416953a869e6f97
Bruno Tomé <ibrunotome@gmail.com>
2026-02-06 12:00:49 -0300

    refactor: remove locale fallback from platform layout

--- src/app/[locale]/(platform)/layout.tsx
+++ src/app/[locale]/(platform)/layout.tsx
@@ -1,5 +1,6 @@
 'use cache'
 
+import type { SupportedLocale } from '@/i18n/locales'
 import { setRequestLocale } from 'next-intl/server'
 import { cacheTag } from 'next/cache'
 import AffiliateQueryHandler from '@/app/[locale]/(platform)/_components/AffiliateQueryHandler'
@@ -7,15 +8,12 @@ import Header from '@/app/[locale]/(platform)/_components/Header'
 import NavigationTabs from '@/app/[locale]/(platform)/_components/NavigationTabs'
 import { FilterProvider } from '@/app/[locale]/(platform)/_providers/FilterProvider'
 import { TradingOnboardingProvider } from '@/app/[locale]/(platform)/_providers/TradingOnboardingProvider'
-import { DEFAULT_LOCALE, SUPPORTED_LOCALES } from '@/i18n/locales'
 import { cacheTags } from '@/lib/cache-tags'
 import { AppProviders } from '@/providers/AppProviders'
 
 export default async function PlatformLayout({ params, children }: LayoutProps<'/[locale]'>) {
   const { locale } = await params
-  const resolvedLocale = SUPPORTED_LOCALES.includes(locale as (typeof SUPPORTED_LOCALES)[number])
-    ? locale
-    : DEFAULT_LOCALE
+  const resolvedLocale = locale as SupportedLocale
   setRequestLocale(resolvedLocale)
   cacheTag(cacheTags.mainTags(resolvedLocale))
 
