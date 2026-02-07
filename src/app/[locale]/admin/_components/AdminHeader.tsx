fab45e418214af5f17c66860799db6cd8ba50281
Bruno Tomé <ibrunotome@gmail.com>
2026-02-07 18:38:36 -0300

    fix: admin header

--- src/app/[locale]/admin/_components/AdminHeader.tsx
+++ src/app/[locale]/admin/_components/AdminHeader.tsx
@@ -3,8 +3,13 @@ import HeaderLogo from '@/components/HeaderLogo'
 
 export default async function AdminHeader() {
   return (
-    <header className="sticky top-0 z-50 bg-background">
-      <div className="container flex h-14 items-center gap-4">
+    <header className="sticky top-0 z-30 bg-background">
+      <div
+        className={`
+          relative z-50 container mx-auto flex min-h-15 w-full items-center gap-4 py-3 pb-1
+          md:min-h-17 md:pb-2
+        `}
+      >
         <HeaderLogo />
         <span>Admin</span>
         <div className="ms-auto flex shrink-0 items-center gap-1 sm:gap-2 lg:gap-4">
