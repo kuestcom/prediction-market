4346dc8c6191e8afd6a112024d44847d3bd4dc92
Bruno Tomé <ibrunotome@gmail.com>
2026-03-09 13:11:38 -0300

    perf: split docs css from globals.css

--- src/app/[locale]/docs/layout.tsx
+++ src/app/[locale]/docs/layout.tsx
@@ -1,5 +1,6 @@
 import { setRequestLocale } from 'next-intl/server'
 import { DocsRootProvider } from '@/components/docs/DocsRootProvider'
+import './docs.css'
 
 export default async function Layout({ params, children }: LayoutProps<'/[locale]/docs'>) {
   const { locale } = await params
