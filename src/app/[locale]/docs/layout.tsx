39b29c45257b83f3b5737be0e7dbbb048e0931d7
Bruno Tomé <ibrunotome@gmail.com>
2026-03-12 15:02:50 -0300

    fix: docs components wrong import path

--- src/app/[locale]/docs/layout.tsx
+++ src/app/[locale]/docs/layout.tsx
@@ -1,5 +1,5 @@
 import { setRequestLocale } from 'next-intl/server'
-import { DocsRootProvider } from '@/components/docs/DocsRootProvider'
+import { DocsRootProvider } from '@/app/[locale]/docs/_components/DocsRootProvider'
 import './docs.css'
 
 export default async function Layout({ params, children }: LayoutProps<'/[locale]/docs'>) {
