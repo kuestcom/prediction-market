c50d8a622b558955636684b6148196dd8c9bc751
Bruno Tomé <ibrunotome@gmail.com>
2026-03-16 08:44:19 -0300

    chore: remove async docs config

--- docs.config.ts
+++ docs.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, defineDocs } from 'fumadocs-mdx/config'
 export const docs = defineDocs({
   dir: 'docs',
   docs: {
-    async: true,
     postprocess: {
       includeProcessedMarkdown: true,
     },
