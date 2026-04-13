6d3b05434d458c37481226d17b41dba975d4a6e7
Bruno Tomé <ibrunotome@gmail.com>
2026-04-13 12:29:42 -0300

    refactor: lint DataTableToolbar after react compiler

--- src/app/[locale]/admin/_components/DataTable.tsx
+++ src/app/[locale]/admin/_components/DataTable.tsx
@@ -119,6 +119,8 @@ export function DataTable<TData, TValue>({
     }
   }, [sorting, onSortChange])
 
+  // TanStack Table intentionally returns non-memoizable handlers.
+  // eslint-disable-next-line react-hooks/incompatible-library
   const table = useReactTable({
     data,
     columns,
