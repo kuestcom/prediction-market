16c771eb87e91dbdb1f0edf80e2cf8e5a71f4267
Bruno Tomé <ibrunotome@gmail.com>
2026-04-21 11:46:22 -0300

    fix: avoid use-cache timeout in home page params flow

--- src/app/[locale]/(platform)/(home)/page.tsx
+++ src/app/[locale]/(platform)/(home)/page.tsx
@@ -1,10 +1,16 @@
-'use cache'
-
+import type { SupportedLocale } from '@/i18n/locales'
 import { setRequestLocale } from 'next-intl/server'
 import HomeContent from '@/app/[locale]/(platform)/(home)/_components/HomeContent'
+
+async function CachedHomePageContent({ locale }: { locale: SupportedLocale }) {
+  'use cache'
+
+  return <HomeContent locale={locale} />
+}
 
 export default async function HomePage({ params }: PageProps<'/[locale]'>) {
   const { locale } = await params
   setRequestLocale(locale)
-  return <HomeContent locale={locale} />
+  const resolvedLocale = locale as SupportedLocale
+  return <CachedHomePageContent locale={resolvedLocale} />
 }
