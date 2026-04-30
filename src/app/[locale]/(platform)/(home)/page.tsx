d8cf560ff4e6d91806e693485922083e5856fffc
Bruno Tomé <ibrunotome@gmail.com>
2026-04-30 15:48:21 -0300

    refactor: remove use cache from HomeContent

--- src/app/[locale]/(platform)/(home)/page.tsx
+++ src/app/[locale]/(platform)/(home)/page.tsx
@@ -1,16 +1,9 @@
-import type { SupportedLocale } from '@/i18n/locales'
 import { setRequestLocale } from 'next-intl/server'
 import HomeContent from '@/app/[locale]/(platform)/(home)/_components/HomeContent'
-
-async function CachedHomePageContent({ locale }: { locale: SupportedLocale }) {
-  'use cache'
-
-  return <HomeContent locale={locale} />
-}
 
 export default async function HomePage({ params }: PageProps<'/[locale]'>) {
   const { locale } = await params
   setRequestLocale(locale)
-  const resolvedLocale = locale as SupportedLocale
-  return <CachedHomePageContent locale={resolvedLocale} />
+
+  return <HomeContent locale={locale} />
 }
