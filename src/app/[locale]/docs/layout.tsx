3c73260e02285d25349b1393082eeb08e3d442dc
Bruno Tomé <ibrunotome@gmail.com>
2026-04-30 16:25:55 -0300

    refactor: move docs css to outside of globals.css

--- src/app/[locale]/docs/layout.tsx
+++ src/app/[locale]/docs/layout.tsx
@@ -1,5 +1,6 @@
 import { setRequestLocale } from 'next-intl/server'
 import { DocsRootProvider } from '@/app/[locale]/docs/_components/DocsRootProvider'
+import './docs.css'
 
 export default async function Layout({ params, children }: LayoutProps<'/[locale]/docs'>) {
   const { locale } = await params
