e7d28586f50f2c4da900275883b6851ffdfb7b65
Bruno Maciel <bruno@maciel.com>
2026-09-26 18:03:26 -0300

    fix: shorten Meld popup reference TTL

--- src/app/[locale]/(platform)/_components/WalletFlow.tsx
+++ src/app/[locale]/(platform)/_components/WalletFlow.tsx
@@ -57,7 +57,7 @@ interface WalletSendMessages {
   withdrawalSubmittedDescription: string
 }
 
-const MELD_CHECKOUT_POPUP_REFERENCE_TTL_MS = 30 * 24 * 60 * 60 * 1_000
+const MELD_CHECKOUT_POPUP_REFERENCE_TTL_MS = 24 * 60 * 60 * 1_000
 
 interface MeldCheckoutPopupReference {
   popup: Window
